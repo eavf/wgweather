36c5790db6d3857d09b8c67bfd97adeb52044f26
Vladimir Frano <eavfeavf@gmail.com>
2022-03-20 00:01:42 +0100

    final

--- src/app/Components/weather-widget-main/weather-widget-main.component.ts
+++ src/app/Components/weather-widget-main/weather-widget-main.component.ts
@@ -11,56 +11,18 @@ export class WeatherWidgetMainComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.WeatherData = {
+      main : {},
+      isDay: true
+    };
     this.getWeatherData();
     console.log(this.WeatherData);
   }
 
   getWeatherData() {
-    let data = JSON.parse(`{
-      "coord": {
-        "lon": -122.08,
-        "lat": 37.39
-      },
-      "weather": [
-        {
-          "id": 800,
-          "main": "Clear",
-          "description": "clear sky",
-          "icon": "01d"
-        }
-      ],
-      "base": "stations",
-      "main": {
-        "temp": 282.55,
-        "feels_like": 281.86,
-        "temp_min": 280.37,
-        "temp_max": 284.26,
-        "pressure": 1023,
-        "humidity": 100
-      },
-      "visibility": 10000,
-      "wind": {
-        "speed": 1.5,
-        "deg": 350
-      },
-      "clouds": {
-        "all": 1
-      },
-      "dt": 1560350645,
-      "sys": {
-        "type": 1,
-        "id": 5122,
-        "message": 0.0139,
-        "country": "US",
-        "sunrise": 1560343627,
-        "sunset": 1560396563
-      },
-      "timezone": -25200,
-      "id": 420006353,
-      "name": "Mountain View",
-      "cod": 200
-      }`);
-      this.setWeatherData(data);
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=brussels&appid=93ff8657ac2d8cc5045389f9e34b62a2')
+    .then(response=>response.json())
+    .then(data=>{this.setWeatherData(data);})
   }
 
   setWeatherData(data){
@@ -72,6 +34,6 @@ export class WeatherWidgetMainComponent implements OnInit {
     this.WeatherData.temp_celcius = (this.WeatherData.main.temp - 273.15).toFixed(0);
     this.WeatherData.temp_min = (this.WeatherData.main.temp_min - 273.15).toFixed(0);
     this.WeatherData.temp_max = (this.WeatherData.main.temp_max - 273.15).toFixed(0);
-    this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0);
+    this.WeatherData.temp_feels_like = (this.WeatherData.main.feels_like - 273.15).toFixed(0); 
   }
 }
